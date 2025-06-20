2150369070423e84ab663dcfb388a587608832e5
RubyMacalma <152216379+RubyMacalma@users.noreply.github.com>
2025-06-20 16:25:45 -0600

    update donate page

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DonateForm from "./DonateForm";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Donate = () => {
-  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
+      if (!user) {
+        window.location.href = "/login";
+      }
+    });
+    // Cleanup the listener on unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <main>
